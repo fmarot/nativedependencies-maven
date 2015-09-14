e099306d87195e7f7524b4e7506096971a33eff8
francois <francois@home>
2015-09-15 00:07:39 +0200

    EXEMPLE Use & configure lwjgl 3 lib instead of lwjgl 2

--- maven-nativedependencies-example/src/main/java/com/googlecode/nativedependencies/example/App.java
+++ maven-nativedependencies-example/src/main/java/com/googlecode/nativedependencies/example/App.java
@@ -1,58 +1,118 @@
 package com.googlecode.nativedependencies.example;
 
-import org.lwjgl.LWJGLException;
 import org.lwjgl.Sys;
-import org.lwjgl.opengl.Display;
-import org.lwjgl.opengl.DisplayMode;
-import org.lwjgl.opengl.GL11;
-
-
-public class App 
-{
-    public static void main( String[] args )
-    {
-    	int targetWidth = 640;
-    	int targetHeight = 480;
-    	
-    	try {
-    		DisplayMode chosenMode = new DisplayMode(targetWidth,targetHeight);
-    		
-    	    Display.setDisplayMode(chosenMode);
-    	    Display.setTitle("Example Maven Natives");
-    	    Display.setFullscreen(false);
-    	    Display.create();
-    	} catch (LWJGLException e) {
-    	    Sys.alert("Error","Unable to create display.");
-    	    System.exit(0);
-    	}
-    	 
-    	GL11.glClearColor(0,0,0,0);
-    	
-    	boolean gameRunning = true;
-    	float pos = 0;
-    	 
-    	int FRAMERATE = 60;
-
-    	while (gameRunning) {
-    		GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_STENCIL_BUFFER_BIT);
-
-    	 
-    	     GL11.glRotatef(0.6f, 0, 0, 1);
-    	     GL11.glBegin(GL11.GL_TRIANGLES);
-    	          GL11.glVertex3f(-0.5f,-0.5f,0);
-    	          GL11.glVertex3f(0.5f,-0.5f,0);
-    	          GL11.glVertex3f(0,0.5f,0);
-    	       
-    	     GL11.glEnd();
-    	 
-    	     Display.update();
-    	     Display.sync(FRAMERATE);
-
-    	     if (Display.isCloseRequested()) {
-    	           gameRunning = false;
-    	           Display.destroy();
-    	           System.exit(0);
-    	     }
-    	}
+import org.lwjgl.glfw.*;
+import org.lwjgl.opengl.*;
+ 
+import java.nio.ByteBuffer;
+ 
+import static org.lwjgl.glfw.Callbacks.*;
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.system.MemoryUtil.*;
+ 
+public class App {
+ 
+    // We need to strongly reference callback instances.
+    private GLFWErrorCallback errorCallback;
+    private GLFWKeyCallback   keyCallback;
+ 
+    // The window handle
+    private long window;
+ 
+    public void run() {
+        System.out.println("Hello LWJGL " + Sys.getVersion() + "!");
+ 
+        try {
+            init();
+            loop();
+ 
+            // Release window and window callbacks
+            glfwDestroyWindow(window);
+            keyCallback.release();
+        } finally {
+            // Terminate GLFW and release the GLFWerrorfun
+            glfwTerminate();
+            errorCallback.release();
+        }
     }
+ 
+    private void init() {
+        // Setup an error callback. The default implementation
+        // will print the error message in System.err.
+        glfwSetErrorCallback(errorCallback = errorCallbackPrint(System.err));
+ 
+        // Initialize GLFW. Most GLFW functions will not work before doing this.
+        if ( glfwInit() != GL11.GL_TRUE )
+            throw new IllegalStateException("Unable to initialize GLFW");
+ 
+        // Configure our window
+        glfwDefaultWindowHints(); // optional, the current window hints are already the default
+        glfwWindowHint(GLFW_VISIBLE, GL_FALSE); // the window will stay hidden after creation
+        glfwWindowHint(GLFW_RESIZABLE, GL_TRUE); // the window will be resizable
+ 
+        int WIDTH = 300;
+        int HEIGHT = 300;
+ 
+        // Create the window
+        window = glfwCreateWindow(WIDTH, HEIGHT, "Hello World!", NULL, NULL);
+        if ( window == NULL )
+            throw new RuntimeException("Failed to create the GLFW window");
+ 
+        // Setup a key callback. It will be called every time a key is pressed, repeated or released.
+        glfwSetKeyCallback(window, keyCallback = new GLFWKeyCallback() {
+            @Override
+            public void invoke(long window, int key, int scancode, int action, int mods) {
+                if ( key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE )
+                    glfwSetWindowShouldClose(window, GL_TRUE); // We will detect this in our rendering loop
+            }
+        });
+ 
+        // Get the resolution of the primary monitor
+        ByteBuffer vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+        // Center our window
+        glfwSetWindowPos(
+            window,
+            (GLFWvidmode.width(vidmode) - WIDTH) / 2,
+            (GLFWvidmode.height(vidmode) - HEIGHT) / 2
+        );
+ 
+        // Make the OpenGL context current
+        glfwMakeContextCurrent(window);
+        // Enable v-sync
+        glfwSwapInterval(1);
+ 
+        // Make the window visible
+        glfwShowWindow(window);
+    }
+ 
+    private void loop() {
+        // This line is critical for LWJGL's interoperation with GLFW's
+        // OpenGL context, or any context that is managed externally.
+        // LWJGL detects the context that is current in the current thread,
+        // creates the ContextCapabilities instance and makes the OpenGL
+        // bindings available for use.
+        GLContext.createFromCurrent();
+ 
+        // Set the clear color
+        glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
+ 
+        // Run the rendering loop until the user has attempted to close
+        // the window or has pressed the ESCAPE key.
+        while ( glfwWindowShouldClose(window) == GL_FALSE ) {
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the framebuffer
+ 
+            glfwSwapBuffers(window); // swap the color buffers
+ 
+            // Poll for window events. The key callback above will only be
+            // invoked during this call.
+            glfwPollEvents();
+        }
+    }
+ 
+    public static void main(String[] args) {
+//    	SharedLibraryLoader.load();
+        new App().run();
+    }
+ 
 }
